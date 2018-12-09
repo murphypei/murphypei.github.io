600a9703c0ab84af35ad5125cf87bf1eb938d324
ChaoPei <peichao0118@gmail.com>
2018-12-09 20:29:57 +0800

    add next theme files

---
+++ themes/next/scripts/helpers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+hexo.extend.helper.register('item_active', function(path, className) {
+  var canonical = this.page.canonical_path;
+  var current = this.url_for(canonical).replace('index.html', '', 'g');
+  var url = this.url_for(path);
+  var result = '';
+
+  if (current.indexOf(path) !== -1) {
+    if (path !== '/' || path === current) {
+      result = ' ' + className;
+    }
+  }
+  return result;
+});
+
+hexo.extend.helper.register('hexo_version', function() {
+  return this.env.version;
+});
